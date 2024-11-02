362531095134518a8abcbbcb5afd2a09b0cb5ed3
Romuald-VT <romualdtchatcho@gmail.com>
2024-11-02 04:34:18 +0100

    first commit

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const {logger} = require('./logging/logger')
 const dbConnection = async()=>{
 
     try{
-        await mongoose.connect(process.env.DB_URL)
+        await mongoose.connect(process.env.MONGO_URL)
         logger.info('connecte a la base de donnees')
     }
     catch(error)
