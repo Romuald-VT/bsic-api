e0834b1268feebbade0dcd4cf3a8ed7811c3136c
Romuald-VT <romualdtchatcho@gmail.com>
2024-11-17 04:06:21 +0100

    ajout de la route des clients !

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const port = process.env.SERVER_PORT || 4000
 
 dbConnection()
 
-app.use(cors())
+app.use(cors({origin:'*'}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(compression())
